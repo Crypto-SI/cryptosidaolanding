d70ca6906e7221a8bf4b660315a1a6ddcd403f99
crypt osi <cryptosixxx@gmail.com>
2025-09-02 13:41:43 +0000

    this should be CryptoSI improvement proposals -- DAO Improvement Proposa

--- src/components/cip-section.tsx
+++ src/components/cip-section.tsx
@@ -32,7 +32,7 @@ export function CipSection() {
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
-            <h2 className="text-3xl font-headline tracking-tighter sm:text-5xl">DAO Improvement Proposals (CIPs)</h2>
+            <h2 className="text-3xl font-headline tracking-tighter sm:text-5xl">CryptoSI Improvement Proposals (CIPs)</h2>
             <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
               The proposal process is fundamental to our decentralized governance, allowing the community to shape the future of the CryptoSI DAO.
             </p>
